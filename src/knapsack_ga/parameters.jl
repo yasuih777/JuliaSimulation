cc7db5c68724023f3b5ab640da8c3aa7e2b4108e
yasuih777 <yuhei.yasui.777@gmail.com>
2024-09-23 23:35:24 +0900

    update: mutation

--- src/knapsack_ga/parameters.jl
+++ src/knapsack_ga/parameters.jl
@@ -6,7 +6,7 @@ Base.@kwdef struct KnapsackGAParameters
     parent_size::Int = 100
     children_size::Int = 10
     item_size::Int = 20
-    mutation_prob::Float16 = 0.25
+    mutation_prob::Float16 = 1.0
 
     knapsack::Dict{String, NTuple{20, Int}} = Dict(
         "cost" => (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
